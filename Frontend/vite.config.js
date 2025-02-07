0af2b857f8f4575fd93c27258cd37b20733aa2e4
Arun Kumar <aruncode49@gmail.com>
2025-02-07 14:09:41 +0530

    changed vite config path in frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://event-management-platform-at8y.onrender.com",
+      "/api": "https://event-management-platform-at8y.onrender.com/",
     },
   },
   plugins: [react()],
